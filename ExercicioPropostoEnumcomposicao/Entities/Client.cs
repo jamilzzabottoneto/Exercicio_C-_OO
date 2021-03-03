b793b8cd29e81acf5bcbc1367d17a61ecd863346
jamilzzabottoneto <jamilzabneto@hotmail.com>
2021-03-03 14:38:46 -0300

    Implementação da Classe Client

---
+++ ExercicioPropostoEnumcomposicao/Entities/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioPropostoEnumcomposicao.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime birthDate { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            this.birthDate = birthDate;
+        }
+    }
+}
