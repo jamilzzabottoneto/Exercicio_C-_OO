39ae9d6caaaa73e6009d925676b5dda021a11a7f
jamilzzabottoneto <jamilzabneto@hotmail.com>
2021-03-03 14:47:40 -0300

    Implementação da Classe Order

---
+++ ExercicioPropostoEnumcomposicao/Entities/Order.cs
@@ -0,0 +1,46 @@
+﻿using ExercicioPropostoEnumcomposicao.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioPropostoEnumcomposicao.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void addItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void removeItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double total()
+        {
+            double sum = 0.0;
+            foreach (OrderItem item in Items)
+            {
+                sum += item.subTotal();
+            }
+            return sum;
+        }
+    }
+}
