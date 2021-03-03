b0f40fdec451258c337ddb485320f7e5e49370a6
jamilzzabottoneto <jamilzabneto@hotmail.com>
2021-03-03 14:41:02 -0300

    Implementação do Enum OrderStatus

---
+++ ExercicioPropostoEnumcomposicao/Enums/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioPropostoEnumcomposicao.Enums
+{
+    enum OrderStatus : int
+    {
+        Pending_Payment,
+        Processing,
+        Shipped,
+        Delivered
+
+    }
+}
